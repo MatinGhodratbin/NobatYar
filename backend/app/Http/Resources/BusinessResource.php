c82fd6ffc977a192b35ee23880def4d724b88bc5
m.matin <mohamadmatin@avina.company>
2026-09-01 17:07:54 +0330

    feat(search): add business api resource

---
+++ backend/app/Http/Resources/BusinessResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BusinessResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'address' => $this->address,
+            'phone' => $this->phone,
+            'logo_path' => $this->logo_path,
+        ];
+    }
+}
